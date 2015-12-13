12434a073114aa9219825b9e9b372274047a84bc
xenogenesi <raziel@eml.cc>
2015-12-13 23:16:45 +0100

    workaround php-fpm/libmysqlclient

--- board/xenogenesi/vbox-vagrant/post_build.sh
+++ board/xenogenesi/vbox-vagrant/post_build.sh
@@ -3,4 +3,8 @@
 TARGETDIR=$1
 BR_ROOT=$PWD
 
-echo "post build do nothing yet"
+#echo "post build do nothing yet"
+
+# FIXME workaround for php-fpm
+cd $TARGETDIR/usr/lib
+ln -sf mysql/libmysqlclient.so.16
