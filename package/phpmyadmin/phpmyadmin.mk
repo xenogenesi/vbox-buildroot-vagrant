01b9bec5d332d31f9652408c5b38bb7caa66eac1
xenogenesi <raziel@eml.cc>
2015-12-14 07:16:18 +0100

    fix php-fpm .php handling and alis for /phpmyadmin/
    FilesMatch \.php$ -> SetHandler proxy (php-fpm)

--- package/phpmyadmin/phpmyadmin.mk
+++ package/phpmyadmin/phpmyadmin.mk
@@ -11,22 +11,21 @@ PHPMYADMIN_VERSION = 4.0.10.11
 PHPMYADMIN_SOURCE = phpMyAdmin-$(PHPMYADMIN_VERSION)-english.tar.xz
 PHPMYADMIN_SITE = http://files.phpmyadmin.net/phpMyAdmin/$(PHPMYADMIN_VERSION)
 
-# $(INSTALL) -D -m 0755 $(@D)/mount.vboxsf \
-# 	$(TARGET_DIR)/sbin
+PHPMYADMIN_ROOTFS_DIR = /var/lib/phpmyadmin
+PHPMYADMIN_TARGET_DIR = $(TARGET_DIR)$(PHPMYADMIN_ROOTFS_DIR)
 
 define PHPMYADMIN_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/htdocs/phpmyadmin
-	-rm -fr $(TARGET_DIR)/usr/htdocs/phpmyadmin/*
-	cd $(@D); tar -cf - . | tar -C $(TARGET_DIR)/usr/htdocs/phpmyadmin -xf -
-	#cp $(TARGET_DIR)/usr/htdocs/phpmyadmin/config.sample.inc.php $(TARGET_DIR)/usr/htdocs/phpmyadmin/config.inc.php
-	sed -e 's/AllowNoPassword'] = false/AllowNoPassword'] = true/' \
-		$(TARGET_DIR)/usr/htdocs/phpmyadmin/config.sample.inc.php \
-		> $(TARGET_DIR)/usr/htdocs/phpmyadmin/config.inc.php
+	mkdir -p $(PHPMYADMIN_TARGET_DIR)
+	-rm -fr $(PHPMYADMIN_TARGET_DIR)/*
+	cd $(@D); tar -cf - . | tar -C $(PHPMYADMIN_TARGET_DIR) -xf -
+	sed -e "s/AllowNoPassword'] = false/AllowNoPassword'] = true/" \
+		$(PHPMYADMIN_TARGET_DIR)/config.sample.inc.php \
+		> $(PHPMYADMIN_TARGET_DIR)/config.inc.php
 endef
 
 
 define PHPMYADMIN_PERMISSIONS
-	/usr/htdocs/phpmyadmin  r  0755  33  33  -  -  -  -  -
+	$(PHPMYADMIN_ROOTFS_DIR)  r  0755  33  33  -  -  -  -  -
 endef
 
 $(eval $(generic-package))
